ab148ff6b687ca280a7ece9a920c9b0f36bb4f3a
Alex Anschuetz <alexanschuetz@gmail.com>
2016-02-16 13:14:55 -0800

    commits album form to master branch to send josh pull request

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var express 	= require('express'),
 	AWS_SECRET_KEY 	= process.env.AWS_SECRET_KEY;
 	S3_BUCKET 	 	= process.env.S3_BUCKET;
 
-
+////comments
 mongoose.connect('mongodb://localhost:27017/project4v2')
 
 // set up middleware
