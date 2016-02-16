edbcc76e9fddd67aec79d56c42289421e23bfe32
Josh <jfadem82@gmail.com>
2016-02-16 00:27:37 -0800

    updates post model

--- server.js
+++ server.js
@@ -28,33 +28,6 @@ app.use('/api', apiRouter) // whenever we get a request starting with /api
 app.listen(port)
 console.log("listening on port " + port)
 
-app.get('/sign_s3', function(req, res){
-	// console.log("get signed request")
-    aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
-    aws.config.update({region: '' , signatureVersion: '' });
-    var s3 = new aws.S3(); 
-    console.log(S3_BUCKET)
-    var s3_params = { 
-        Bucket: S3_BUCKET, 
-        Key: req.query.file_name, 
-        Expires: 60, 
-        ContentType: req.query.file_type, 
-        ACL: 'public-read'
-    }; 
-    s3.getSignedUrl('putObject', s3_params, function(err, data){ 
-        if(err){ 
-            console.log(err); 
-        }
-        else{ 
-            var return_data = {
-                signed_request: data,
-                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name 
-            };
-            res.write(JSON.stringify(return_data));
-            res.end();
-        } 
-    });
-});
 
 
 
