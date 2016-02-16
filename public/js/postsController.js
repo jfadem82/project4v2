ddb0f721f20400ff8809e26418c88664f152c2be
Alex Anschuetz <alexanschuetz@gmail.com>
2016-02-15 16:03:18 -0800

    saving 0 index id as string as userid

--- public/js/postsController.js
+++ public/js/postsController.js
@@ -8,19 +8,20 @@ function PostsController (postsFactory){
 	var vm = this;
 	vm.api = postsFactory
 	vm.posts = []
-	vm.userinfo = {}
+	vm.userid = {}
+	vm.userinfo = vm.api.getuserinfo()
+		.success(function(res){
+			console.log("here is the res " + JSON.stringify(res))
+			vm.userid = JSON.stringify(res[0]["_id"])
+			console.log("here is the vm.userinfo " + vm.userinfo)
+		})
 	vm.newPost = {}
 	vm.api.list()
 		.success(function(res){
 			vm.posts = res
 		})
 	vm.addPost = function(make,model,year, userid){
-		vm.api.getuserinfo()
-		.success(function(res){
-			console.log("here is the res " + JSON.stringify(res))
-			vm.userinfo = JSON.stringify(res[0]["_id"])
-			console.log("here is the vm.userinfo " + vm.userinfo)
-		})	
+			
 		var data = {make:make, model:model, year:year, userid:userid}
 		console.log("user id is " + userid)
 		vm.api.addPost(data)
