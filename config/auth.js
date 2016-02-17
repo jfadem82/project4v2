24abd8cecefe660f1ed07f5d313f54be5cac4c86
Alex Anschuetz <alexanschuetz@gmail.com>
2016-02-16 16:13:37 -0800

    Delete auth.js

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-		'amazonAuth'   :  {
-		'clientID'		:'AKIAI3OUEPNVJZNOLYNQ',
-		'clientSecret'  :'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',
-		'callbackURL'	:'project4v2'
-	},
-};
