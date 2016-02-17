a257cb86a7a7b7d56dbbd88304d16d31c7c83e4b
Alex Anschuetz <alexanschuetz@gmail.com>
2016-02-17 11:19:52 -0800

    improves user settings page

--- public/js/editFactories.js
+++ public/js/editFactories.js
@@ -7,37 +7,16 @@ editFactory.$inject = ['$http']
 function editFactory($http){
 	var usertoeditURL = 'http://localhost:3000/api/users'
 	var url = 'http://localhost:3000/api/posts'
+	var edits = {}
 
 	editFactory.list = function(){
-		return $http.get(url)
+		return $http.get(usertoeditURL)
 	}
 
 	editFactory.updateinfo = function(userId,data){
+		console.log("we in the factory bruh")
 		return $http.patch(usertoeditURL + '/' + userId, data)
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-	editFactory.updateinfo = function(email, password, name, bio){
-			console.log("token is " + $window.localStorage.getItem('token'))
-		// return $http.post()
-	}
+	return edits
 }
-
-	authFactory.signup = function(email, password, name, bio){
-		return $http.post('http://localhost:3000/api/users', {
-			email: email,
-			password: password,
-			name: name,
-			bio: bio
-		})
-	}
